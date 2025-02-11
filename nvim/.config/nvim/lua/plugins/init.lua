8e9c6666d0d1908a578dcba1c144ad0762fc1d39
zlendo1 <zlendo1@etf.unsa.ba>
2025-02-11 21:13:53 +0100

    added tree-sitter modules for render-markdown

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -22,6 +22,9 @@ return {
         "vimdoc",
         "html",
         "css",
+        "markdown",
+        "markdown_inline",
+        "latex",
       },
     },
   },
@@ -92,7 +95,10 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
     },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
     opts = {},
   },
 }
