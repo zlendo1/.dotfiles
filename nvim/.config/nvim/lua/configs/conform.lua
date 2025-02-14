f4eafa1a3be79b93683d0b34ed56a6411494d9a5
zlendo1 <zlendo1@etf.unsa.ba>
2025-02-14 20:54:53 +0100

    added markdown formatting

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -10,6 +10,7 @@ local options = {
     bib = { "bibtex-tidy" },
     rust = { "rustfmt" },
     python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+    markdown = { "mdformat", "mdsf" },
   },
 
   format_on_save = {
