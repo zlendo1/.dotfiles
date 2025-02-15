ad53d028317ea4cffb8c1a70183bb76b7d47bb50
zlendo1 <zlendo1@etf.unsa.ba>
2025-02-15 14:35:35 +0100

    added firefox to browser in markdown preview

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -107,6 +107,7 @@ return {
     build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_browser = "firefox"
     end,
     ft = { "markdown" },
   },
