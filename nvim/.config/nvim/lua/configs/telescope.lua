29f636a55e5f1d422f68514c85e201566e650475
zlendo1 <zlendo1@etf.unsa.ba>
2025-04-09 19:29:47 +0200

    added telescope selection feature

--- nvim/.config/nvim/lua/configs/telescope.lua
+++ nvim/.config/nvim/lua/configs/telescope.lua
@@ -1,4 +1,38 @@
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+local function multiopen(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+
+  if not vim.tbl_isempty(multi) then
+    actions.close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        local path = vim.fn.fnameescape(j.path)
+        if j.lnum ~= nil then
+          vim.cmd(string.format("silent! edit +%d %s", j.lnum, path))
+        else
+          vim.cmd(string.format("silent! edit %s", path))
+        end
+      end
+    end
+  else
+    actions.select_default(prompt_bufnr)
+  end
+end
+
 local options = {
+  defaults = {
+    mappings = {
+      i = {
+        ["<CR>"] = multiopen,
+      },
+      n = {
+        ["<CR>"] = multiopen,
+      },
+    },
+  },
   extensions = {
     fzf = {
       override_generic_sorter = true,
