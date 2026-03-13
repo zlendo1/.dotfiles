4ad9d66c9ad4ae7d5d1f582b5575c5ff5a2ab204
zlendo1 <zlendo1@etf.unsa.ba>
2026-03-13 13:49:41 +0100

    removed unused configs

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,17 +5,6 @@ return {
     opts = require "configs.conform",
   },
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "nvim-java/nvim-java",
-    },
-    config = function()
-      require("java").setup()
-      require "configs.lspconfig"
-    end,
-  },
-
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
@@ -77,72 +66,5 @@ return {
         -- Configuration here, or leave empty to use defaults
       }
     end,
-  },
-
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  },
-
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_browser = ""
-    end,
-    ft = { "markdown" },
-  },
-
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        events = "InsertEnter",
-        config = function()
-          require("copilot").setup {
-            suggestion = { enabled = false },
-            panel = { enabled = false },
-          }
-        end,
-      },
-    },
-    opts = {},
-    config = function(_, opts)
-      local copilot_cmp = require "copilot_cmp"
-      copilot_cmp.setup(opts)
-      vim.lsp.on_attach(function()
-        copilot_cmp._on_insert_enter {}
-      end, "copilot")
-    end,
-  },
-
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" },
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    opts = {
-      -- See Configuration section for options
-    },
-    keys = {
-      { "<leader>zc", ":CopilotChat<CR>", mode = "n", desc = "open copilot chat" },
-      { "<leader>ze", ":CopilotChatExplain<CR>", mode = "v", desc = "explain code" },
-      { "<leader>zr", ":CopilotChatReview<CR>", mode = "v", desc = "review code" },
-      { "<leader>zf", ":CopilotChatFix<CR>", mode = "v", desc = "fix code issues" },
-      { "<leader>zo", ":CopilotChatOptimize<CR>", mode = "v", desc = "optimize code" },
-      { "<leader>zd", ":CopilotChatDocs<CR>", mode = "v", desc = "generate docs" },
-      { "<leader>zt", ":CopilotChatTests<CR>", mode = "v", desc = "generate tests" },
-    },
   },
 }
